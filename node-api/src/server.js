a5a89f8a11e464b8df6d37e816171dfc82a42847
Alexander <aragonantar@gmail.com>
2025-01-18 10:48:17 -0500

    Update server.js

--- node-api/src/server.js
+++ node-api/src/server.js
@@ -16,7 +16,7 @@ server.use("/api", authRoutes);
 server.get("/", (req, res) => {
   return res.status(200).json({
     ok: true,
-    message: "root route, tested deploy 4",
+    message: "root route, tested deploy 5",
   });
 });
 server.get("/server/healthCheck", (req, res) => {
